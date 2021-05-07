cb06cfd7a29dccd3dd7ec11c07eafec2556ec384
Sybille Peters <sypets@gmx.de>
2021-05-07 19:45:09 +0200

    [TASK] Add CI script for local checking

---
+++ Build/Scripts/ci.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 7.4 and composer latest
+
+# abort on error
+set -e
+
+echo "cgl"
+Build/Scripts/runTests.sh -p 7.4 -s cgl -n
+
+echo "composer install"
+Build/Scripts/runTests.sh -p 7.4 -s composerInstallMax
+
+echo "composer validate"
+Build/Scripts/runTests.sh -p 7.4 -s composerValidate
+
+echo "lint"
+Build/Scripts/runTests.sh -p 7.4 -s lint
+
+echo "phpstan"
+Build/Scripts/runTests.sh -p 7.4 -s phpstan -e "-c ../Build/phpstan_composerInstallMax.neon"
+
+echo "Unit tests"
+Build/Scripts/runTests.sh -p 7.4 -s unit
+
+echo "functional tests"
+Build/Scripts/runTests.sh -p 7.4 -d mariadb -s functional
