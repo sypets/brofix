172378ea04bab8fd855784ed4884b4e3e8d4f3f6
Sybille Peters <sybille.peters@uni-oldenburg.de>
2023-06-10 13:30:14 +0200

    v4.4.0-dev

---
+++ Classes/Linktype/ForbiddenLinktype.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace Sypets\Brofix\Linktype;
+
+/**
+ * Generic link types for not allowed link types
+ */
+class MiscLinktype extends AbstractLinktype
+{
+
+}
