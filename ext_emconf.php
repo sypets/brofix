375e78e6caf7f61ff750cc5a1ceef5b9edbcd18d
Sybille Peters <sypets@gmx.de>
2024-10-27 09:01:43 +0100

    [FEATURE] Make ready for v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '6.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.4-12.9.99',
+            'typo3' => '12.4.4-13.9.99',
         ],
         'conflicts' => [],
         'suggests' => [
