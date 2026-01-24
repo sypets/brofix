d48fd21c12c851aa5f431a8dc3025215f9e2fa00
Sybille Peters <sypets@gmx.de>
2026-01-24 12:40:26 +0100

    [BUGFIX] Replace deprecated addPageTsConfig for v13 support
    Resolves: #460

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,14 +8,6 @@ use TYPO3\CMS\Backend\Form\FormDataProvider\TcaColumnsRemoveUnused;
 defined('TYPO3') or die();
 
 (function () {
-    // ------------------
-    // load page TSconfig
-    // ------------------
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        "@import 'EXT:brofix/Configuration/TsConfig/Page/pagetsconfig.tsconfig'"
-    );
-
     // -----
     // Fluid
     // -----
