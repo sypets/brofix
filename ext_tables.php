36fc10387c1639938fae899a76cae1ae781920e7
Sybille Peters <sypets@gmx.de>
2026-08-02 22:27:59 +0200

    [FEATURE] Add option for showPageCalloutBrokenLinksExist
    There are now 3 options for extension configuration
    showPageCalloutBrokenLinksExist ("Show message in
    page module if broken links exist on page")
    
    - Never
    - Depends on user settings
    - Always
    
    Only if it is set to "Depends on user settings", is it possible
    to change the setting in the user settings.
    
    This influeces whether broken links are displayed in the page
    module (requires page_callouts).

--- ext_tables.php
+++ ext_tables.php
@@ -8,12 +8,25 @@ defined('TYPO3') or die();
 // ----------------
 
 $lll = 'LLL:EXT:brofix/Resources/Private/Language/locallang_be_usersettings.xlf';
-// make it possible to turn off page_callouts in page module (this is also influenced by extension configuration showPageCalloutBrokenLinksExist)
-$GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_brofix_showPageCalloutBrokenLinksExist'] = [
-    'label' => $lll . ':usersettings.pagemodule.showPageCalloutBrokenLinksExist',
-    'type' => 'check',
-    'default' => '1',
-];
-ExtensionManagementUtility::addFieldsToUserSettings(
-    '--div--;' . $lll . ':usersettings.brofix.tab,tx_brofix_showPageCalloutBrokenLinksExist',
-);
+/**
+ * is moved to Configuration/TCA/Overrides/be_users.php for TYPO3 >= v14
+ * @todo Remove here when support for TYPO3 v13 is dropped
+ */
+// directly access extension configuration so class does not need to be initialized
+$showPageCalloutBrokenLinksExist = (int)($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['brofix']['showPageCalloutBrokenLinksExist'] ?? 1);
+if ($showPageCalloutBrokenLinksExist === 1) {
+    // BE user settings
+    // ----------------
+
+    $lll = 'LLL:EXT:brofix/Resources/Private/Language/locallang_be_usersettings.xlf';
+    // make it possible to turn off page_callouts in page module (this is also influenced by extension configuration showPageCalloutBrokenLinksExist)
+    $GLOBALS['TYPO3_USER_SETTINGS']['columns']['tx_brofix_showPageCalloutBrokenLinksExist'] = [
+        'label' => $lll . ':usersettings.pagemodule.showPageCalloutBrokenLinksExist',
+        'type' => 'check',
+        'default' => '1',
+    ];
+    ExtensionManagementUtility::addFieldsToUserSettings(
+        '--div--;' . $lll . ':usersettings.brofix.tab,tx_brofix_showPageCalloutBrokenLinksExist',
+    );
+}
+
